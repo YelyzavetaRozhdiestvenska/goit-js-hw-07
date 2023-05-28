f10fb1f08f75be7e97aa3af3b255722481bdc1d4
YelyzavetaRozhdiestvenska <alirr@ukr.net>
2023-05-28 18:15:51 +0200

    lightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,36 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+const galleryMarcup = createMarkupGallery(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', galleryMarcup);
+
+function createMarkupGallery(galleryItems) {
+
+    return galleryItems
+        .map(({ original, preview, description }) => 
+     `<li class = "gallery__item">
+  <a class = "gallery__link"  href = "${original}">
+    <img
+      class="gallery__image"
+      src = "${preview}"
+      alt = "${description}"
+    />
+  </a>
+</li >`
+ ).join('');
+}
+
+var lightbox = new SimpleLightbox('.gallery a', {
+   captionsData: 'alt',
+   captionDelay: 250,
+});
+
+
+
+
+
+
+
